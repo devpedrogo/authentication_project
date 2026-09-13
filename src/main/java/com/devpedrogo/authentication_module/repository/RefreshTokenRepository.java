f548717b352121b46730605c4a1b19612c24656e
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-09-13 00:49:15 -0300

    criando o jwt service

--- src/main/java/com/devpedrogo/authentication_module/repository/RefreshTokenRepository.java
+++ src/main/java/com/devpedrogo/authentication_module/repository/RefreshTokenRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface IRefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     
     Optional<RefreshToken> findByToken(String token);
     
--- src/main/java/com/devpedrogo/authentication_module/repository/UserRepository.java
+++ src/main/java/com/devpedrogo/authentication_module/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface IUserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     
     Optional<User> findByEmail(String email);
     
