29395ab85fc97674f31a11dd6eaf24c1c09a7ad4
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-09-15 00:26:11 -0300

    cria dtos de request e response

---
+++ src/main/java/com/devpedrogo/authentication_module/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.devpedrogo.authentication_module.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail em formato inválido")
+    String email,
+
+    @NotBlank(message = "A senha é obrigatória")
+    String password
+) {}
