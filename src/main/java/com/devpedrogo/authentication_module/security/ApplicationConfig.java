2a359320ea13f241ef53741b75d9fa245646b33c
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-09-15 00:15:03 -0300

    fix bean AuthenticationProvider

--- src/main/java/com/devpedrogo/authentication_module/security/ApplicationConfig.java
+++ src/main/java/com/devpedrogo/authentication_module/security/ApplicationConfig.java
@@ -28,11 +28,10 @@ public class ApplicationConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
-    }
+    }    
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
